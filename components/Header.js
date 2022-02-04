dd3015fb5a73905c7e417a98053d220ae67adba7
Matt Keane <mattpkeane@gmail.com>
2022-02-04 11:26:48 -0600

    added colors to constants

--- components/Header.js
+++ components/Header.js
@@ -3,6 +3,7 @@ import {
     Text,
     StyleSheet,
 } from 'react-native'
+import colors from '../constants/colors'
 
 export default function Header(props) {
     return (
@@ -17,7 +18,7 @@ const styles = StyleSheet.create({
         width: '100%',
         height: 90,
         paddingTop: 36,
-        backgroundColor: '#0B3D91',
+        backgroundColor: colors.primary,
         alignItems: 'center',
         justifyContent: 'center',
     },
---
+++ constants/colors.js
@@ -0,0 +1,6 @@
+// Holds app's default colors
+
+export default {
+    // NASA "Midnight Blue"
+    primary: '#0B3D91',
+}
