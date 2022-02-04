df68f85bbc6c3f3a1df17b5793ff7b54f1fcc86e
Matt Keane <mattpkeane@gmail.com>
2022-02-04 14:32:40 -0600

    app makes requests to api and logs json

--- App.js
+++ App.js
@@ -4,30 +4,44 @@ import {
   View,
   Text, 
 } from 'react-native';
-import { useState } from 'react'
+import { 
+  useState,
+  useEffect,
+} from 'react'
 import DatePicker from 'react-native-date-picker'
+
+const { API_KEY } = process.env
 
 import Header from './components/Header'
 
 export default function App() {
-  const [date, setDate] = useState(new Date())
+  const [selectedDate, setSelectedDate] = useState(new Date())
   const [open, setOpen] = useState(false)
 
   const handleConfirm = dateInput => {
     setOpen(false)
-    setDate(dateInput)
+    setSelectedDate(dateInput)
   }
+
+  useEffect(() => {
+    // putting date into correct format
+    const formattedDate = selectedDate.toISOString().split('T')[0]
+    const url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${formattedDate}&end_date=${formattedDate}&api_key=${API_KEY}`
+    fetch(url)
+      .then(response => response.json())
+      .then(json => console.log(json))
+  }, [selectedDate])
 
   return (
     <View style={ styles.container }>
       <Header title="NASA NEO" />
       <Button title="OPEN" onPress={ () => setOpen(true) } />
-      <Text>{ date.toLocaleDateString() }</Text>
+      <Text>{ selectedDate.toLocaleDateString() }</Text>
       <DatePicker
         modal
         mode="date"
         open={ open }
-        date={ date }
+        date={ selectedDate }
         onConfirm={ handleConfirm }
         onCancel={ () => setOpen(false) }        
       />
